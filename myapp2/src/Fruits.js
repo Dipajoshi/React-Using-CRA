81c661a39f2ee2f57a5a25db645ec04864d3e4c1
DIPAJOSHI <joshideepa988@gmail.com>
2024-03-24 01:54:51 +0545

    chosing fruit

---
+++ myapp2/src/Fruits.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import foods from "./food";
+import {choice, remove} from "./helper";
+class Fruits extends Component{
+    render(){
+        let fruit=choice(foods);
+        let remainingFoods = remove(foods, fruit);
+        return(
+            <div>
+                <p>Total Fruits:{foods.length}</p>
+                <p>They are:{foods.join}</p>
+                <p>The chosen fruit is:{fruit}</p>
+                <p>The remaining fruit are:{remainingFoods.length}</p>
+                <p>They are:{remainingFoods.join}</p>
+
+            </div>
+        )
+    }
+}
+export default Fruits;
+
