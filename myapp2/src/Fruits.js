726774a9bd9c28daeb544b65cf09795ee2b3aefe
DIPAJOSHI <joshideepa988@gmail.com>
2024-03-24 11:09:15 +0545

    join function added

--- myapp2/src/Fruits.js
+++ myapp2/src/Fruits.js
@@ -8,10 +8,10 @@ class Fruits extends Component{
         return(
             <div>
                 <p>Total Fruits:{foods.length}</p>
-                <p>They are:{foods.join}</p>
+                <p>They are:{foods.join(",")}</p>
                 <p>The chosen fruit is:{fruit}</p>
                 <p>The remaining fruit are:{remainingFoods.length}</p>
-                <p>They are:{remainingFoods.join}</p>
+                <p>They are:{remainingFoods.join(",")}</p>
 
             </div>
         )
