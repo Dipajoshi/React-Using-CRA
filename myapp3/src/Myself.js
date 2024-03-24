720437be03a31450697032220341a0b86b43a843
DIPAJOSHI <joshideepa988@gmail.com>
2024-03-24 11:28:36 +0545

    rendering image without importing

--- myapp3/src/Myself.js
+++ myapp3/src/Myself.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-import dipa from "./dipa.jpg";
-console.log(dipa);
+// import dipa from "./dipa.jpg";
+
 
 class Myself extends Component { 
     render() {
         return (
             <div>
                 <p>This is me.</p>
-                <img src={dipa} alt="deepa" className="image"/>
+                {/* <img src={dipa} alt="deepa" className="image"/> */}
+                <img src={require("./dipa.jpg")} alt="deepa" className="image"/>
                
             </div>
         );
