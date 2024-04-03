7c563af2860bf75935b761f1007e1b9442aca82e
DIPAJOSHI <joshideepa988@gmail.com>
2024-04-03 22:15:12 +0545

    errors Corrected

--- FormDemo1/src/App.js
+++ FormDemo1/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import ContactForm from "./ContactForm";
 class App extends Component{
   render(){
-    return(<div>
+    return(<div style={{textAlign : "center"}}>
       <ContactForm/>
     </div>);
   }
--- FormDemo1/src/ContactForm.js
+++ FormDemo1/src/ContactForm.js
@@ -4,11 +4,12 @@ class ContactForm extends Component{
     constructor(props){
         super(props);
         this.state={username:""};
-        this.handleChange=this.state.handleChange.bind(this);
-    }
-    handleChange(){
+        this.handleChange=this.handleChange.bind(this);
         
     }
+    handleChange(e){
+        this.setState({username:e.target.value});
+        }
 
     render(){
        return(
@@ -16,8 +17,12 @@ class ContactForm extends Component{
         <h2>Contact form</h2>
         <form>
             <label htmlFor="usrname" >Name</label>
-            <input type="text" id="usrname"></input>
-
+            <input type="text" id="usrname"
+             value={this.state.username}
+              onChange={this.handleChange}>
+                </input>
+         
+            
         </form>
         <p>You typed:{this.state.username}</p>
         </div>
